ad113a0a642be8074454fae3ebb89279ec1dd9d1
hoainamdk <hoainamdk2002@gmail.com>
2024-06-19 15:21:03 +0700

    e

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='WEBSITE-CICD'>
+  <BrowserRouter basename='/WEBSITE-CICD'>
     <App />
   </BrowserRouter>
 );
